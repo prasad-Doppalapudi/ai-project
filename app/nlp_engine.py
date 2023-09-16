97be38d30524afcacb8afffd53cf8c961dc8cc37
prasad doppalapudi <paysy09@gmail.com>
2023-09-15 22:38:00 -0400

    librabies update

--- app/nlp_engine.py
+++ app/nlp_engine.py
@@ -1,58 +1,95 @@
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
-import string
-
-# Download NLTK data (if not already downloaded)
-nltk.download('stopwords')
-nltk.download('punkt')
-
-# Sample question understanding function
-def understand_question(question_text):
-    # Tokenize the question into words
-    words = word_tokenize(question_text.lower())
-    
-    # Remove stopwords and punctuation
-    stop_words = set(stopwords.words("english"))
-    words = [word for word in words if word not in stop_words and word not in string.punctuation]
-    
-    # Perform additional NLP processing here if needed
-    
-    # Return the cleaned and processed words
-    return words
-
-# Sample function for checking if a question is valid or relevant
-def is_valid_question(question):
-    # Implement your logic to determine if the question is valid or relevant
-    # This could involve checking against a predefined list of valid questions or using NLP techniques
-    
-    # For this example, we assume all questions are valid
-    return True
-
 # Sample function for categorizing the type of question (e.g., technical, behavioral)
 def categorize_question(question):
     # Implement your logic to categorize the question based on its content
     # This could involve keyword matching or more advanced NLP techniques
     
-    # For this example, we categorize all questions as "General"
-    return "General"
+    # Sample categories for a senior QA engineer
+    if "stlc" in question:
+        return "STLC (Software Testing Life Cycle)"
+    elif "regression testing" in question:
+        return "Regression Testing"
+    elif "qa process" in question:
+        return "QA Process"
+    elif "test automation" in question:
+        return "Test Automation"
+    elif "test strategy" in question:
+        return "Test Strategy"
+    elif "defect tracking" in question:
+        return "Defect Tracking"
+    elif "test plan" in question:
+        return "Test Plan"
+    elif "test case design" in question:
+        return "Test Case Design"
+    elif "test environment setup" in question:
+        return "Test Environment Setup"
+    elif "continuous integration" in question:
+        return "Continuous Integration (CI)"
+    elif "test management tools" in question:
+        return "Test Management Tools"
+    else:
+        return "General"
 
-# Main function for question understanding
-def process_question(question_text):
-    # Understand the question (tokenize, remove stopwords, etc.)
-    cleaned_question = understand_question(question_text)
+# Sample dictionary of interview questions and answers
+interview_questions = {
+    "STLC (Software Testing Life Cycle)": [
+        "What is STLC, and what are its phases?",
+        "Explain the importance of requirement analysis in STLC.",
+    ],
+    "Regression Testing": [
+        "What is regression testing, and why is it essential?",
+        "How do you prioritize test cases for regression testing?",
+    ],
+    "QA Process": [
+        "Can you describe the QA process you follow in your projects?",
+        "What steps do you take to ensure the quality of software?",
+    ],
+    "Test Automation": [
+        "What are the benefits of test automation?",
+        "Which test automation tools have you used, and why?",
+    ],
+    "Test Strategy": [
+        "How do you create a test strategy for a project?",
+        "What factors do you consider when designing a test strategy?",
+    ],
+    "Defect Tracking": [
+        "How do you track and manage defects in your projects?",
+        "What is the defect life cycle, and how do you prioritize defects?",
+    ],
+    "Test Plan": [
+        "What components are typically included in a test plan?",
+        "How do you ensure that a test plan is comprehensive?",
+    ],
+    "Test Case Design": [
+        "Explain different test case design techniques you've used.",
+        "How do you ensure that test cases cover both positive and negative scenarios?",
+    ],
+    "Test Environment Setup": [
+        "What challenges have you faced in setting up test environments?",
+        "How do you ensure the consistency and availability of test environments?",
+    ],
+    "Continuous Integration (CI)": [
+        "What is continuous integration, and how does it benefit testing?",
+        "Describe your experience with integrating testing into CI/CD pipelines.",
+    ],
+    "Test Management Tools": [
+        "Which test management tools have you worked with?",
+        "How do you use test management tools to track and report test progress?",
+    ],
+}
+
+# Main function for answering interview questions
+def answer_interview_question(question_category, question_index):
+    category = categorize_question(question_category)
+    if category in interview_questions:
+        questions = interview_questions[category]
+        if question_index < len(questions):
+            return questions[question_index]
     
-    # Check if the question is valid or relevant
-    if not is_valid_question(cleaned_question):
-        return "I'm sorry, I don't understand the question."
-    
-    # Categorize the question
-    question_category = categorize_question(cleaned_question)
-    
-    # Implement more advanced question understanding logic as needed
-    
-    # Return the processed question and its category
-    return {
-        "cleaned_question": cleaned_question,
-        "category": question_category
-    }
+    return "I don't have a specific answer for that question at the moment."
+
+# Example usage:
+category = "QA" "software testing" "Senior QA Engineer" "Test Engineer"
+question_index = 0
+answer = answer_interview_question(category, question_index)
+print("Question:", category)
+print("Answer:", answer)
